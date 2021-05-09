5b21eac52d5a6684b785df578259c75705fc8f78
artbat6 <arthurbatesdev@gmail.com>
2021-05-09 16:04:14 -0500

    created lib directory and files

--- app.js
+++ app.js
@@ -75,3 +75,66 @@ class DAL {
              
         });
     }
+
+    async getAllEmployeeNames(){
+
+        let conn = await newConnect();    
+        
+        return new Promise((resolve,reject)=>{          
+            
+            let query = "select id, first_name,last_name from employee";
+            
+            conn.query(query, (err,res)=>{
+                if (err) throw err;
+                conn.end();                
+                resolve(res);
+            });    
+             
+        });
+    }
+
+    async getAllEmployeesFullData(){
+
+        let conn = await newConnect();    
+        
+        return new Promise((resolve,reject)=>{          
+            
+            let query = `select  e.id,
+            first_name,
+            last_name,
+            r.title,
+            r.salary,
+            (select concat(e2.first_name,' ',e2.last_name) from employee as e2 where e.manager_id = e2.id) as 'manager',
+            d.name
+            from employee as e
+            left join role as r on e.role_id = r.id
+            left join department as d on r.department_id = d.id`;
+            
+            conn.query(query, (err,res)=>{
+                if (err) throw err;
+                conn.end();                
+                resolve(res);
+            });    
+             
+        });
+    }
+
+    async getAllManagerNames(){
+
+        let conn = await newConnect();    
+        
+        return new Promise((resolve,reject)=>{          
+            
+            let query = `select distinct id,
+            concat(first_name, ' ',last_name) as 'name'          
+            from employee
+            where id in (select manager_id from employee where manager_id is not null)`;
+            
+            conn.query(query, (err,res)=>{
+                if (err) throw err;
+                conn.end();                
+                resolve(res);
+            });    
+             
+        });
+    }
